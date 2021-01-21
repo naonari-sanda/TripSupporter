aa4ce6daa2473ff149060ca47f1bf3ea8fee5c70
Nonari Sanda <mkg_sandy@icloud.com>
2021-01-21 11:44:39 +0900

    シーダーファイルの調整をしました。

--- server/database/migrations/2020_10_04_120857_create_countries_table.php
+++ server/database/migrations/2020_10_04_120857_create_countries_table.php
@@ -25,7 +25,7 @@ class CreateCountriesTable extends Migration
             $table->integer('gdp')->comment('GDP');
             $table->integer('happiness')->comment('幸福度');
             $table->string('map')->nullable()->comment('地図');
-            $table->string('covid', 250)->nullable()->comment('コロナ情報');
+            $table->string('covid', 300)->nullable()->comment('コロナ情報');
             $table->string('detail', 500)->comment('詳細');
             $table->string('comment', 20)->nullable()->comment('コメント');
             $table->timestamps();
