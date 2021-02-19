5ed954a4e9e28a20ace52d878b57fa5cc07123d0
Nonari Sanda <mkg_sandy@icloud.com>
2021-02-19 16:59:28 +0900

    config.ymlにテストコードを追記しました

--- server/config/database.php
+++ server/config/database.php
@@ -63,6 +63,20 @@ return [
             ]) : [],
         ],
 
+        'circle_test' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
