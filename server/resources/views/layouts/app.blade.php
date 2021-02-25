5052414b0df4e66cbc0fe2372fe0ff91c15c471f
Nonari Sanda <mkg_sandy@icloud.com>
2021-02-25 11:55:35 +0900

    head内にmetaタグを 追加

--- server/resources/views/layouts/app.blade.php
+++ server/resources/views/layouts/app.blade.php
@@ -2,8 +2,11 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-
+    <!-- 必須タグ -->
+    <meta charset=”UTF-8″>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>@yield('title')</title>
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
